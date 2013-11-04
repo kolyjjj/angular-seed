6561cf6f0b82fb0e44330c13d8a8ef2052023e10
kolyjjj <me.koly.li@gmail.com>
2013-11-04 16:38:41 +0800

    add click me directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,10 +2,19 @@
 
 /* Directives */
 
-
 angular.module('myApp.directives', []).
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }])
+  .directive("clickMe", function(){
+  	return function(scope, elem, attrs){
+  		elem.bind("click", function(){
+  			if (elem.hasClass("background-green"))
+  				elem.removeClass("background-green");	
+  			else 
+  				elem.addClass("background-green");
+  		})
+  	};
+  });
