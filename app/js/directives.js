cb1dc75c8a668689343592c26f0af7af68b9ccb4
kolyjjj <me.koly.li@gmail.com>
2013-11-04 16:43:32 +0800

    injected rootscope and log out the scopes

--- app/js/directives.js
+++ app/js/directives.js
@@ -6,15 +6,20 @@ angular.module('myApp.directives', []).
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
+      console.log(scope, 'appversion');
     };
   }])
-  .directive("clickMe", function(){
+  .directive("clickMe", function($rootScope){
   	return function(scope, elem, attrs){
   		elem.bind("click", function(){
   			if (elem.hasClass("background-green"))
   				elem.removeClass("background-green");	
   			else 
   				elem.addClass("background-green");
-  		})
+  		});
+  		console.log($rootScope, "rootscope");
+  		console.log(scope, 'scope');
+  		console.log(attrs, 'attrs');
   	};
+
   });
